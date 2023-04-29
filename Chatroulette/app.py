271310a296818bed53c3165a19dba17a5323a2ee
Vincent Dahmen <4dahmen@informatik.uni-hamburg.de>
2023-04-29 17:30:11 +0200

    templates: adds first dummy template

--- Chatroulette/app.py
+++ Chatroulette/app.py
@@ -3,22 +3,18 @@ This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app
 
 
-@app.route('/')
-def hello():
-    """Renders a sample page."""
-    return "Hello World!"
+@app.route('/create/player')
+def new_player_guide():
+    """Show site on which user can be created"""
+    return render_template("create_player.html")
 
-@app.route('/troll')
-def roll():
-    """Rick rolls ppls"""
-    return '<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
 
 if __name__ == '__main__':
     import os
