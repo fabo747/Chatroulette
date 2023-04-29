fe6c4b174a5d7714238281848643a0a8a6848fbc
Vincent Dahmen <4dahmen@informatik.uni-hamburg.de>
2023-04-29 17:16:53 +0200

    app: fixes typo

--- Chatroulette/app.py
+++ Chatroulette/app.py
@@ -15,7 +15,7 @@ def hello():
     """Renders a sample page."""
     return "Hello World!"
 
-@app.route('troll')
+@app.route('/troll')
 def roll():
     """Rick rolls ppls"""
     return '<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
